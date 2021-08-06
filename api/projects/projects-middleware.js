2e6074d092b718fc5836339a3da16e789ea90138
Marcos Saavedra <marcsaav96@gmail.com>
2021-08-06 09:55:40 -0700

    [GET] Project by ID Endpoint implemented

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,31 @@
 // add middlewares here related to projects
+
+const Projects = require('./projects-model')
+
+const checkProjectId = (req, res, next) => {
+    let { id } = req.params
+    Projects.get(id)
+        .then((project) => {
+            if(project) {
+                req.project = project
+                next()
+            } else {
+                next({ status: 404, message: 'Project not found'})
+            }
+        })
+}
+
+const checkProjectBody = (req, res, next) => {
+    let { description, name } = req.body
+
+    if(!description || !name) {
+        next({ status: 400, message: 'Please provide Name, Description of Project Please'})
+    } else {
+        next()
+    }
+}
+
+module.exports = {
+    checkProjectId,
+    checkProjectBody
+}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -5,10 +5,21 @@ const router = express.Router()
 
 const Projects = require('./projects-model')
 
+const { checkProjectId, checkProjectBody } = require('./projects-middleware')
+
 router.get('/', async (req, res, next) => {
     try {
         const projects = await Projects.get()
         res.status(200).json(projects)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.get('/:id', checkProjectId, (req, res, next) => {
+    try {
+        const project = req.project
+        res.status(200).json(project)
     } catch(err) {
         next(err)
     }
