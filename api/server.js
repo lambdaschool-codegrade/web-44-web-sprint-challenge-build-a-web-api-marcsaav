37a6c720bdf6e48d0685317cb3cbd9dc031530aa
Marcos Saavedra <marcsaav96@gmail.com>
2021-08-06 07:48:00 -0700

    Server instantiated

--- api/server.js
+++ api/server.js
@@ -1,9 +1,29 @@
 const express = require('express');
+const cors = require('cors')
+const helmet  = require('helmet')
 const server = express();
 
 // Configure your server here
+
+server.use(cors())
+
+server.use(helmet())
+
+server.use(express.json())
+
 // Build your actions router in /api/actions/actions-router.js
+
+const actionsRouter = require('./actions/actions-router')
+
+server.use('/api/actions', actionsRouter)
+
 // Build your projects router in /api/projects/projects-router.js
+
+const projectsRouter = require('./projects/projects-router')
+
+server.use('/api/projects', projectsRouter)
+
+
 // Do NOT `server.listen()` inside this file!
 
 module.exports = server;
